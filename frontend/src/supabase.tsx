41a55bf3b8a0b9ed4453c6cdaf99bff8aeed4f69
eharris733 <elliotmharris@gmail.com>
2025-07-10 21:05:01 -0400

    now it works

--- frontend/src/supabase.tsx
+++ frontend/src/supabase.tsx
@@ -4,4 +4,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 // const supabaseServiceRoleKey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    db: { schema: 'brooklyn' }
+})
