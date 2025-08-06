b8b9724bf3488dcd2f8dfd7300403e821146bb26
eharris733 <elliotmharris@gmail.com>
2025-08-05 23:04:44 -0400

    hoepfully one of these changes works

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,16 @@ const config = getDefaultConfig(__dirname);
 // Add platform-specific extensions
 config.resolver.platforms = ['native', 'web', 'ios', 'android'];
 
+// Ensure font files are properly handled
+config.resolver.assetExts = [
+  ...config.resolver.assetExts,
+  'ttf',
+  'otf',
+  'woff',
+  'woff2'
+];
+
+// Add asset plugins for fonts
+config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
+
 module.exports = config;
