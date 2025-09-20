25674f2f6558812fedea5505639aaf27b79a7705
eharris733 <elliotmharris@gmail.com>
2025-09-20 02:31:37 -0400

    some more quality changes yay

---
+++ src/utils/eventUtils.ts
@@ -0,0 +1,8 @@
+/**
+ * Utility functions and constants for event-related operations
+ */
+
+/**
+ * Fallback text to display when an event has no description
+ */
+export const EVENT_NO_DESCRIPTION_FALLBACK = 'No description available';
