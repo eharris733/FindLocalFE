95bcbbd0989fc677d39a4bf4d98d00f26443ce5f
eharris733 <elliotmharris@gmail.com>
2025-10-21 16:56:33 -0400

    maybe this was missing?

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -3,7 +3,27 @@ import { Html, Head, Main, Scripts } from 'expo-router';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* 
+          Load icon fonts statically for production builds.
+          These @font-face rules reference the font files bundled by Expo.
+          The require() calls ensure Expo bundles the fonts into /assets/.
+        */}
+        <style dangerouslySetInnerHTML={{
+          __html: `
+            @font-face {
+              font-family: 'Ionicons';
+              src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf')}) format('truetype');
+              font-display: swap;
+            }
+            @font-face {
+              font-family: 'antdesign';
+              src: url(${require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/AntDesign.ttf')}) format('truetype');
+              font-display: swap;
+            }
+          `
+        }} />
+      </Head>
       <body>
         <Main />
         <Scripts />
